21ce094adf5d37c71c5d8fdecc4c730278bf235a
Gajan Nagaraj <thegajannagaraj@gmail.com>
2014-10-20 19:55:53 -0700

    Finished Prject??!??!?!?!?

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
     });
+    $("#bib").click(function () {
+        $("#bib-full").fadeIn('slow');
+    });
+    $("#close").click(function () {
+        $("#bib-full").fadeOut('slow');
+    });
 });
